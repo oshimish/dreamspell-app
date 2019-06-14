e9f5817290be45aa5755156f4b5bfd9edee2d425
Roman Oshimish <oshimish@gmail.com>
2019-06-14 17:34:30 +0300

    [-] missing kin on Year page bug [+] green portals on years page

--- src/Pages/YearPage/YearPage.tsx
+++ src/Pages/YearPage/YearPage.tsx
@@ -1,12 +1,12 @@
 // Vendor
-import React from 'react';
+import React from "react";
 
-import * as g from 'dreamspell-math';
+import * as g from "dreamspell-math";
 
 // Internal
-import './styles.css';
-import { WaveSpell, Moon } from '../../Visuals';
-import { Moment } from 'moment';
+import "./styles.css";
+import { WaveSpell, Moon } from "../../Visuals";
+import { Moment } from "moment";
 // Setup
 
 /**
@@ -15,29 +15,23 @@ import { Moment } from 'moment';
  */
 
 class YearPage extends React.Component<{
-  gdate: g.DreamDate
+  gdate: g.DreamDate;
 }> {
-
-
   render() {
     const selKin = this.props.gdate.kin;
     const selDate = this.props.gdate;
     // todo: add getWavespell to math
-    let wavespellStart = selDate
-      .moment
-      .clone()
-      .add(-selDate.dayOfYear - 1, 'd');
-    let iterator = (moment: Moment) => moment
-      .add(28, 'd')
-      .clone();
+    let wavespellStart = selDate.moment.clone().add(-selDate.dayOfYear, "d");
+    let iterator = (moment: Moment) => moment.add(28, "d").clone();
     return (
-      <div className="year-page" >
+      <div className="year-page">
         <h2>Год</h2>
 
         <WaveSpell
           from={wavespellStart}
           iterator={iterator}
-          render={i => (<Moon firstDay={g.dreamdate(i)} selDate={selDate} />)} />
+          render={i => <Moon firstDay={g.dreamdate(i)} selDate={selDate} />}
+        />
       </div>
     );
   }
--- src/Visuals/Kin/Kin.tsx
+++ src/Visuals/Kin/Kin.tsx
@@ -1,8 +1,8 @@
 // Vendor
-import React from 'react';
-import Sign from './Sign';
-import SignTone from './SignTone';
-import * as g from 'dreamspell-math';
+import React from "react";
+import Sign from "./Sign";
+import SignTone from "./SignTone";
+import * as g from "dreamspell-math";
 
 // Setup
 
@@ -10,17 +10,14 @@ import * as g from 'dreamspell-math';
  * @class Kin
  * @description Brief description
  */
-export class Kin extends React.PureComponent<{ kin: g.Kin }> {
-
-  render() {
-    let kin = this.props.kin;
-    return (
-      <div className="kin">
-        <SignTone tone={kin.tone} />
-        <Sign sign={kin.sign} />
-      </div>
-    );
-  }
-}
+export const Kin = (props: { kin: g.Kin }) => {
+  let kin = props.kin;
+  return (
+    <div className="kin">
+      <SignTone tone={kin.tone} />
+      <Sign sign={kin.sign} />
+    </div>
+  );
+};
 
 export default Kin;
